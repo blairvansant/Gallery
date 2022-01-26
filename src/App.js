c06a1387394f2dc4aae3156e882a74d542e6c98a
gitblairshub <blairsarlo@gmail.com>
2022-01-25 21:43:39 -0500

    UploadForm component created.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Title from "./comps/Title"; 
+import UploadForm from "./comps/UploadForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Title />
+        <UploadForm />
       </header>
     </div>
   );
