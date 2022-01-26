2e84f8e24d3581d7865d736d462bebd1073b1ab0
gitblairshub <blairsarlo@gmail.com>
2022-01-26 13:44:43 -0500

    Firebase Storage custom hook

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import { projectStorage  } from '../firebase/config';
+
+// passing in file from UploadForm
+const useStorage = (file) => {
+  // 3 pieces of state - 'progress' of the upload, 'error' from upload, and 'url' for img from storage after upload 
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  //useEffect hook takes in a function as paramater, the second argument is the dependencies. The function fires everytime the dependency changes.
+  useEffect(() => {
+    // references - for where the file should be saved once uploaded
+    const storageRef = projectStorage.ref(file.name);
+
+    storageRef.put(file).on('state_changeed', (snapshot) => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      setProgress(percentage);
+    }, (err) => {
+       setError(err);
+    }, async () => {
+      const url = await storageRef.getDownloadURL();
+      setUrl(url);
+    })
+  }, [file]);
+
+  return { progress, url, error }
+}
+
+export default useStorage;
