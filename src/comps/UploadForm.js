daf478a4353ae2e96d7cdba859b45df864a11ad3
gitblairshub <blairsarlo@gmail.com>
2022-01-25 22:30:23 -0500

    Added event handler to the upload form submission

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const UploadForm = () => {
+
+  const changeHandler = (e) => {
+    console.log("it changed")
+  }
   return (
     <form>
-      <input type="file" />
+      <input type="file" onChange={changeHandler}/>
     </form>
   )
 }
