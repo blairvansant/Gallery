0387b890a758e90e2500850df300227600e6e354
gitblairshub <blairsarlo@gmail.com>
2022-01-25 23:56:55 -0500

    Add validation for selected file types

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,13 +1,34 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 
 const UploadForm = () => {
+// for storing the file in local state
+const [file, setFile] = useState(null);
+const [error, setError] = useState(null);
+
+// allowed file type
+const types = ['image/png', 'image/jpeg'];
 
   const changeHandler = (e) => {
-    console.log("it changed")
+    // for accessing the file that the user has selected
+    let selected = e.target.files[0];
+    
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      // resets error after selecting valid file
+      setError('')
+    } else {
+      setFile(null);
+      setError('Please select an image file (png or jpeg)')
+    }
   }
   return (
     <form>
       <input type="file" onChange={changeHandler}/>
+      <div className="output">
+        { error && <div className="error">{ error }</div>}
+        { file && <div>{ file.name }</div>}
+      </div>
     </form>
   )
 }
