2e57d607101ce5ec3cce01c6c3490106e37c54df
gitblairshub <blairsarlo@gmail.com>
2022-01-31 14:53:10 -0500

    Completed hook for firestore and started on modal for displaying larger uploaded images

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,20 +1,44 @@
-import { useState, useEffect } from 'react';
-import { projectFirestore } from '../firebase/config';
+// import { useState, useEffect } from 'react';
+// import { projectFirestore } from '../firebase/config';
+
+// const useFirestore = (collection) => {
+//   const [docs, setDocs] = useState([]);
+
+//   useEffect(() => {
+//     const unsub = projectFirestore.collection(collection)
+//     .orderBy('createdAt', 'desc')
+//     .onSnapshot((snap) => {
+//       let documents = [];
+//       snap.forEach(doc => {
+//         documents.push({...doc.data(), id: doc.id})
+//       });
+//       setDocs(documents);
+//     });
+
+//     return () => unsub();
+//   }, [collection])
+
+//   return { docs };
+// }
+
+// export default useFirestore;
+
+import { useState, useEffect } from "react";
+import { projectFirestore } from "../firebase/config";
 
 const useFirestore = (collection) => {
-  const [docs, setDocs] = useState([]);
+  const [docs, setDocs] =useState([]);
 
   useEffect(() => {
     const unsub = projectFirestore.collection(collection)
-    .orderBy('createdAt', 'desc')
-    .onSnapshot((snap) => {
-      let documents = [];
-      snap.forEach(doc => {
-        documents.push({...doc.data(), id: doc.id})
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snap) => {
+        let documents = [];
+        snap.forEach(doc => {
+          documents.push({...doc.data(), id: doc.id})
+        });
+        setDocs(documents);
       });
-      setDocs(documents);
-    });
-
     return () => unsub();
   }, [collection])
 
